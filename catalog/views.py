76ff2be6420ef2faec9486e803bd6569faeec90c
Roman_Soldatov <soldatov.rom4ik@gmail.com>
2026-06-16 17:29:52 +0800

    add files

---
+++ catalog/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f'{name}, Ваше сообщение получено, вскоре с Вами свяжется специалист.')
+    return render(request, 'contacts.html')
