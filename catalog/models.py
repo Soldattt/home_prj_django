56c434a83ca46b41b949b8631b705d1018df8016
Roman_Soldatov <soldatov.rom4ik@gmail.com>
2026-06-22 23:28:50 +0800

    add files .env.sample, .flake8, migration and modified settings.py, urls.py/config, urls.py/catalog, views.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование категории')
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/image', blank=True, null=True, verbose_name='Картинка продукта')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'category']
+
+    def __str__(self):
+        return f'Продукт {self.name}, цена {self.price} руб., категория продукта {self.category}'
+
+
+
