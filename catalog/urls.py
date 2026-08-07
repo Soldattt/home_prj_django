38106f50742f4510bfaaaef5a20333b8af78a126
Roman_Soldatov <soldatov.rom4ik@gmail.com>
2026-08-08 00:31:35 +0800

    modified base.html, contacts.html, inc_menu.html, product_detail.html, urls.py, views.py and add forms.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 
-from . import views
+from .views import ProductListView, ProductDetailView, ContactsFormView
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path("", views.product_list, name="products_list"),
-    path("catalog/<int:id>/", views.product_detail, name="product_detail"),
-    path("contacts/", views.contacts, name="contacts"),
+    path("", ProductListView.as_view(), name="products_list"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("contacts/", ContactsFormView.as_view(), name="contacts"),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
 
+from django.views.generic import ListView, DetailView, FormView
+
+from catalog.forms import ContactForm
 from catalog.models import Product
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "products_list.html", context)
 
-def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/products_list.html"
+    context_object_name = "products"
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        return HttpResponse(
-            f"{name}, Ваше сообщение получено, вскоре с Вами свяжется специалист."
-        )
-    return render(request, "contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
+
+class ContactsFormView(FormView):
+    template_name = 'products/contacts.html'
+    form_class = ContactForm
+    def form_valid(self, form):
+        name = form.cleaned_data['name']
+        return HttpResponse(f'Спасибо, {name}! Мы с вами свяжемся в ближайшее время.')
 
 
 
